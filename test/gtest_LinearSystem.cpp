1e6d8ad9fcc7bdaff71c2514e6862ea5338c4777
Ignácio R <ignacio@poli.ufrj.br>
2019-07-23 15:38:30 -0300

    add more gtests, 3/5 in total

--- test/gtest_LinearSystem.cpp
+++ test/gtest_LinearSystem.cpp
@@ -89,8 +89,6 @@ void printProgress(int width, float progress)
 
 TEST(LinearSystemTest, testUpdatesTakeTooLong)
 {
-    std::cout << "[TEST] update timeout" << std::endl;
-
     Poly num(3), den(3);
     num << 0, 0, 1;
     den << 1, 2, 1;
@@ -120,6 +118,77 @@ TEST(LinearSystemTest, testUpdatesTakeTooLong)
     // THESE LIMITS HAVE BEEN HARD CODED!
     EXPECT_FALSE( (y1 - y2).cwiseAbs().minCoeff() < 0.139545 && (y1 - y2).cwiseAbs().maxCoeff() > 0.142949 ) << "the filter values do not look right after calling update" << std::endl;
 }
+
+TEST(LinearSystemTest, testNumberOfFiltersSimple)
+{
+    Poly num(2), den(2);
+    num << 0, 1;
+    den << 1, 1;
+    LinearSystem sys(num, den);
+    sys.useNFilters(3);
+
+    Eigen::MatrixXd ydy0(3,1);
+    ydy0 << 1,
+            1,
+            1;
+    Eigen::VectorXd u0 = Eigen::VectorXd::Zero(3,1);
+    sys.setInitialConditions(u0, ydy0);
+    sys.setInitialTime(0);
+
+    Eigen::VectorXd input(3);
+    input << 2,2,2;
+    Eigen::VectorXd out = sys.update(input, LinearSystem::getTimeFromSeconds(sys.getSampling()));
+    double delta = 1e-15;
+    EXPECT_FALSE( std::abs(out(0) - out(1)) > delta || std::abs(out(1) != out(2)) > delta ) << "filters output differ" << std::endl;
+}
+
+TEST(LinearSystemTest, testNumberOfFilters)
+{
+    YAML::Node doc = YAML::LoadFile("test/test_LinearSystem.yml");
+
+    int progress_width = 50;
+    float progress_max = doc.size();
+    double Ts;
+    for (unsigned i = 0; i < doc.size(); i++)
+    {
+        printProgress(progress_width, i / progress_max);
+
+        int n, order;
+        LinearSystem tustin_ls;
+        LinearSystem _a;
+        LinearSystem _b;
+        Eigen::VectorXd u;
+        Eigen::VectorXd y_tustin_data;
+        Eigen::VectorXd y_fwd_data;
+        Eigen::VectorXd y_bwd_data;
+
+        GetYmlData(doc[i],n,order,tustin_ls,_a,_b,u,y_tustin_data,y_fwd_data,y_bwd_data,Ts);
+
+        Eigen::VectorXd u_i(1);
+        Eigen::VectorXd y_tustin(n);
+        //
+        Eigen::VectorXd num, den;
+
+        // update filters
+        Time step = LinearSystem::getTimeFromSeconds(Ts);
+        Time time = step;
+        for (int k = 0; k < n; ++k)
+        {
+            u_i(0) = u(k);
+
+            y_tustin(k) = tustin_ls.update(u_i, time)(0);
+            time += step;
+        }
+
+        double delta = 1e-5;
+        double maxError;
+
+        maxError = (y_tustin_data - y_tustin).cwiseAbs().maxCoeff();
+        EXPECT_FALSE(maxError > delta) << "y_tustin error. Max error = " << maxError<< std::endl;
+    }
+    printProgress(progress_width, 1.0);
+}
+
 int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
