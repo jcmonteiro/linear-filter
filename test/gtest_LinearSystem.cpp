012e86c0363048cb59978608d9aee09da219c4a0
Ignácio R <ignacio@poli.ufrj.br>
2019-07-23 15:59:06 -0300

    implement gtest framework 5/5

--- test/gtest_LinearSystem.cpp
+++ test/gtest_LinearSystem.cpp
@@ -68,25 +68,6 @@ void GetYmlData(const YAML::Node &node, int& n, int &order, LinearSystem &ls_tus
     ls_bwd.setInitialTime(0);
 }
 
-void printProgress(int width, float progress)
-{
-    std::cout << "[";
-    int pos = width * progress;
-    for (int i = 0; i < width; ++i)
-    {
-        if (i < pos)
-            std::cout << "=";
-        else if (i == pos)
-            std::cout << ">";
-        else
-            std::cout << " ";
-    }
-    std::cout << "] " << int(progress * 100.0) << " %\r";
-    std::cout.flush();
-    if (progress == 1.0)
-        std::cout << std::endl;
-}
-
 TEST(LinearSystemTest, testUpdatesTakeTooLong)
 {
     Poly num(3), den(3);
@@ -146,13 +127,9 @@ TEST(LinearSystemTest, testNumberOfFilters)
 {
     YAML::Node doc = YAML::LoadFile("test/test_LinearSystem.yml");
 
-    int progress_width = 50;
-    float progress_max = doc.size();
     double Ts;
     for (unsigned i = 0; i < doc.size(); i++)
     {
-        printProgress(progress_width, i / progress_max);
-
         int n, order;
         LinearSystem tustin_ls;
         LinearSystem _a;
@@ -186,7 +163,125 @@ TEST(LinearSystemTest, testNumberOfFilters)
         maxError = (y_tustin_data - y_tustin).cwiseAbs().maxCoeff();
         EXPECT_FALSE(maxError > delta) << "y_tustin error. Max error = " << maxError<< std::endl;
     }
-    printProgress(progress_width, 1.0);
+}
+
+TEST(LinearSystemTest, testLinearSystem)
+{
+    YAML::Node doc = YAML::LoadFile("test/test_LinearSystem.yml");
+
+    double Ts;
+    for (unsigned i = 0; i < doc.size(); i++)
+    {
+        int n, order;
+        LinearSystem tustin_ls;
+        LinearSystem fwd_ls;
+        LinearSystem bwd_ls;
+        
+        Eigen::VectorXd u;
+        Eigen::VectorXd y_tustin_data;
+        Eigen::VectorXd y_fwd_data;
+        Eigen::VectorXd y_bwd_data;
+
+        
+        GetYmlData(doc[i],n,order,tustin_ls,fwd_ls,bwd_ls,u,y_tustin_data,y_fwd_data,y_bwd_data,Ts);
+
+        // Should change this to consider the exact same initial conditions as the
+        // used to generate the test results
+        Eigen::MatrixXd u0 = Eigen::MatrixXd::Constant(1, order, u(0));
+        //
+        Eigen::VectorXd u_i(1);
+        Eigen::VectorXd y_tustin(n);
+        Eigen::VectorXd y_fwd(n);
+        Eigen::VectorXd y_bwd(n);
+
+        // update filters
+        Time step = LinearSystem::getTimeFromSeconds(Ts);
+        Time time = step;
+        for (int i = 0; i < n; i++)
+        {
+            u_i(0) = u(i);
+            y_tustin(i) = tustin_ls.update(u_i, time)(0);            
+            y_fwd(i) = fwd_ls.update(u_i, time)(0);
+            y_bwd(i) = bwd_ls.update(u_i, time)(0);
+
+            time += step;
+        }
+
+        double delta = 1e-5;
+        double maxError;
+
+        maxError = (y_tustin_data - y_tustin).cwiseAbs().maxCoeff();
+        EXPECT_FALSE(maxError > delta) << "y_tustin error. Max error = " << maxError << std::endl;
+
+        maxError = (y_fwd_data - y_fwd).cwiseAbs().maxCoeff();
+        EXPECT_FALSE(maxError > delta) << "y_fwd error. Max error = " << maxError << std::endl;
+        
+        maxError = (y_bwd_data - y_bwd).cwiseAbs().maxCoeff();
+        EXPECT_FALSE(maxError > delta) << "y_bwd error. Max error = " << maxError << std::endl;
+    }
+}
+
+TEST(LinearSystemTest, testSamplingTime)
+{
+    double damp = 0.7;
+    double wc = 2 * M_PI;
+    double wn = cutoff2resonant(wc, damp);
+
+    Eigen::VectorXd num(1), den(3);
+    num << wn*wn;
+    den << 1, 2*wn*damp, wn*wn;
+
+    LinearSystem model_ts_a(num, den, 0.003, TUSTIN);
+    LinearSystem model_ts_b(num, den, 0.005, TUSTIN);
+    LinearSystem model_ts_c(num, den, 0.011, TUSTIN);
+
+    Eigen::MatrixXd init_out(1, 2), init_in(1, 2);
+    init_out << 0, 0;
+    init_in  << 1, 1;
+    //
+    model_ts_a.setInitialTime(0);
+    model_ts_a.setInitialConditions(init_in, init_out);
+    //
+    model_ts_b.setInitialTime(0);
+    model_ts_b.setInitialConditions(init_in, init_out);
+    //
+    model_ts_c.setInitialTime(0);
+    model_ts_c.setInitialConditions(init_in, init_out);
+
+    Time step = 5000L;
+    Time time = 0L;
+    double time_settling = wn / 4;
+    Time timeout = 1000000L * (2 * time_settling);
+    Eigen::VectorXd ref(1);
+    ref << 1;
+    unsigned int samples = timeout / step + 1, k = 0;
+    Eigen::MatrixXd data(samples, 3);
+    while (time < timeout)
+    {
+        model_ts_a.update(ref, time);
+        model_ts_b.update(ref, time);
+        model_ts_c.update(ref, time);
+
+        data(k, 0) = model_ts_a.getOutput()[0];
+        data(k, 1) = model_ts_b.getOutput()[0];
+        data(k, 2) = model_ts_c.getOutput()[0];
+
+        time += step;
+        ++k;
+    }
+    data.conservativeResize(k, Eigen::NoChange);
+
+    double overshoot = 1 + std::exp(-M_PI * damp/std::sqrt(1-damp*damp));
+    for (unsigned int k = 0; k < 3; ++k)
+    {
+        double max = data.col(k).array().abs().maxCoeff();
+        double error_over = std::abs(max - overshoot);
+        double max_diff = (data.colwise() - data.col(k)).array().abs().maxCoeff();
+        double tol_over = 0.0015; // 0.15%
+        double tol_diff = 0.04;
+        EXPECT_FALSE( error_over > tol_over ) << "Measured step-response does not match the nominal one. Overshoot error = " << error_over << " , tol = " << tol_over << std::endl;
+        EXPECT_FALSE( max_diff > tol_diff ) << "Step-responses differ from each other. Max diff = " << max_diff << " , tol = " << tol_diff << std::endl;
+    }
 }
 
 int main(int argc, char **argv)
